115929e84c325c0e808a2fcb28d76a29c693b452
OMAR <omohammed2000@gmail.com>
2024-09-02 04:20:29 +0300

    Initial commit

---
+++ src/test/java/org/example/electronicsecommerce/ElectronicsEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.electronicsecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
